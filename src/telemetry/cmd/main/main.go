46cb564494f8fe369b32beb855fbac1502153165
Nigel Tatschner <ntatschner@gmail.com>
2024-05-26 17:23:36 +0100

    added package names to files

--- src/telemetry/cmd/main/main.go
+++ src/telemetry/cmd/main/main.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"github.com/ntatschner/Tatux.Telemetry/src/telemetry/pkg/logging"
-	"github.com/ntatschner/Tatux.Telemetry/src/telemetry/pkg/telemetry/adapters"
-	"github.com/ntatschner/Tatux.Telemetry/src/telemetry/pkg/telemetry/domain"
 )
 
 func main() {
 	logger := logging.NewStdLogger()
-
-	telemetryService := domain.NewTelemetryService(logger)
-
-	adapter := adapters.NewTelemetryAdapter(telemetryService, logger)
 
 	logger.Info("Starting telemetry service")
 
