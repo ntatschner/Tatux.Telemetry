279585bbdfa4e186cace2b478bd0aa5b188c7258
Nigel Tatschner <ntatschner@gmail.com>
2024-05-26 19:07:53 +0100

    Added more logic to the main api

--- src/telemetry/pkg/api/handler.go
+++ src/telemetry/pkg/api/handler.go
@@ -1,1 +1,15 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/ntatschner/Tatux.Telemetry/src/telemetry/pkg/logging"
+)
+var logger = logging.NewStdLogger()
+
+func HealthCheck(c *gin.Context) {
+	logger.Info("Health check request received")
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
--- src/telemetry/pkg/api/router.go
+++ src/telemetry/pkg/api/router.go
@@ -1,1 +1,18 @@
 package api
+
+import (
+	ginrouter "github.com/gin-gonic/gin"
+
+	"github.com/ntatschner/Tatux.Telemetry/src/telemetry/logging"
+	"github.com/ntatschner/Tatux.Telemetry/src/telemetry/pkg/telemetry/ports/incoming"
+)
+
+func NewRouter() *ginrouter.Engine {
+    router := ginrouter.Default()
+
+    // Define routes
+    router.GET("/api/ping", HealthCheck)
+	router.POST("/api/telemetry", incoming.HandleJSONPayload)
+
+    return router
+}
